8d8e6a65fbabde2ab6f287301457a7cb955b536b
Wayne W <Tree@Winter-Mac.local>
2015-07-19 14:46:16 +0800

    ffmpeg-libs support x86_64

--- script/config_i386.sh
+++ script/config_i386.sh
@@ -1,13 +1,22 @@
 #!/bin/tcsh -f
 #!export PATH=$PATH:/Applications/Xcode.app/Contents/Developer/usr/bin:/usr/bin
 
+#use x86_64 instead of i386, which is available since iPhone5s
+set ARCH=x86_64
+
+#base on iOS8.2 SDK
+set BASE_SDK_VER="8.2"
+set BASE_SDK_PLAT="iPhoneSimulator"
+set SYS_ROOT="/Applications/Xcode.app/Contents/Developer/Platforms/${BASE_SDK_PLAT}.platform/Developer/SDKs/${BASE_SDK_PLAT}${BASE_SDK_VER}.sdk"
+
+#at least iOS5.0
 set MIN_IOS_VER=5.0
 set EXTFLAGS_LD="-miphoneos-version-min=${MIN_IOS_VER}"
 
 set xbinDir="/Applications/Xcode.app/Contents/Developer/usr/bin"
 
 set targetDir_root="./ffmpeg-libs"
-set targetDir="${targetDir_root}/i386"
+set targetDir="${targetDir_root}/${ARCH}"
 if (! -d ${targetDir_root} ) mkdir ${targetDir_root}
 if (! -d $targetDir ) mkdir $targetDir
 
@@ -24,13 +33,13 @@ $xbinDir/make clean
 ./configure \
 --cc=/Applications/Xcode.app/Contents/Developer/usr/bin/gcc \
 --as='/usr/local/bin/gas-preprocessor.pl /Applications/Xcode.app/Contents/Developer/usr/bin/gcc' \
---sysroot=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator8.2.sdk \
+--sysroot="${SYS_ROOT}" \
 --target-os=darwin \
---arch=i386 \
---cpu=i386 \
---extra-cflags='-arch i386' \
---extra-ldflags="-arch i386 ${EXTFLAGS_LD} -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator8.2.sdk" \
---prefix=compiled/i386 \
+--arch="${ARCH}" \
+--cpu="${ARCH}" \
+--extra-cflags="-arch ${ARCH}" \
+--extra-ldflags="-arch ${ARCH} ${EXTFLAGS_LD} -isysroot ${SYS_ROOT}" \
+--prefix="compiled/${ARCH}" \
 --enable-cross-compile \
 --enable-nonfree \
 --enable-gpl \
@@ -46,14 +55,14 @@ $xbinDir/make clean
 
 $xbinDir/make && $xbinDir/make install
 
-mv ./libavcodec/libavcodec.a $targetDir
-mv ./libavdevice/libavdevice.a $targetDir
-mv ./libavformat/libavformat.a $targetDir
-mv ./libavutil/libavutil.a $targetDir
-mv ./libswscale/libswscale.a $targetDir
-mv ./libavfilter/libavfilter.a $targetDir
-mv ./libpostproc/libpostproc.a $targetDir
-mv ./libswresample/libswresample.a $targetDir
+mv ./libavcodec/libavcodec.a $targetDir/libavcodec-${ARCH}.a
+mv ./libavdevice/libavdevice.a $targetDir/libavdevice-${ARCH}.a
+mv ./libavformat/libavformat.a $targetDir/libavformat-${ARCH}.a
+mv ./libavutil/libavutil.a $targetDir/libavutil-${ARCH}.a
+mv ./libswscale/libswscale.a $targetDir/libswscale-${ARCH}.a
+mv ./libavfilter/libavfilter.a $targetDir/libavfilter-${ARCH}.a
+mv ./libpostproc/libpostproc.a $targetDir/libpostproc-${ARCH}.a
+mv ./libswresample/libswresample.a $targetDir/libswresample-${ARCH}.a
 
 mkdir $headerDir/libavcodec
 cp ./libavcodec/*.h $headerDir/libavcodec
