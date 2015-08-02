c9c5cf2d0f1e3d2f10a7a48d2ce997077211abdc
Wayne W <Tree@Winter-Mac.local>
2015-08-02 17:37:42 +0800

    commit the missing files

---
+++ ffmpeg4iOS/Codec/Video/ffmpegVideoDecode.m
@@ -0,0 +1,67 @@
+//
+//  ffmpegVideoDecode.m
+//  ffmpeg4iOS
+//
+//  Created by Wayne W on 15/8/2.
+//  Copyright (c) 2015年 github.com/henern. All rights reserved.
+//
+
+#import "ffmpegVideoDecode.h"
+#import "ehm.h"
+#import "ffmpegYUVBuffer.h"
+
+@interface DEF_CLASS(ffmpegVideoDecode) ()
+{
+    REF_CLASS(ffmpegYUV420PBuffer) m_bufYUV;
+}
+
+@end
+
+@implementation DEF_CLASS(ffmpegVideoDecode)
+
++ (BOOL)supportCodec:(AVCodecContext *)ctxCodec
+{
+    return YES;
+}
+
+- (int)decodePacket:(AVPacket *)pkt
+          yuvBuffer:(__autoreleasing id<WWYUVBuffer> *)yuvBuffer
+              codec:(AVCodecContext *)ctxCodec
+           finished:(int *)finished
+{
+    BOOL ret = YES;
+    int err = ERR_SUCCESS;
+    
+    AVFrame *avfDecoded = av_frame_alloc();
+    
+    CPRA(ctxCodec);
+    CPRA(pkt);
+    CPRA(yuvBuffer);
+    CPRA(finished);
+    
+    err = avcodec_decode_video2(ctxCodec, avfDecoded, finished, pkt);
+    CBRA(err >= 0);
+    
+    // buffer is re-usable
+    if (!m_bufYUV)
+    {
+        m_bufYUV = [[DEF_CLASS(ffmpegYUV420PBuffer) alloc] initWithCodec:ctxCodec];
+    }
+    CPRA(m_bufYUV);
+    
+    err = [m_bufYUV attach2frame:avfDecoded fmt:ctxCodec->pix_fmt];
+    CBRA(err == ERR_SUCCESS);
+    
+    *yuvBuffer = m_bufYUV;
+    
+ERROR:
+    if (avfDecoded)
+    {
+        av_frame_free(&avfDecoded);
+        avfDecoded = NULL;
+    }
+    
+    return err;
+}
+
+@end
