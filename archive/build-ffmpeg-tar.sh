01a87a86f0ce3d7583e3f5dd10233efaa65097f5
Wayne W <Tree@Winter-Mac.local>
2015-09-13 00:05:11 +0800

    add build_ffmpeg-tar.sh

---
+++ archive/build-ffmpeg-tar.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+VERSION=$1
+ARCHS="i386 armv7 arm64"
+
+# working folders
+CURRENTPATH=`pwd`
+PROJECT_DIR="${CURRENTPATH}/.."
+SCRIPTS_DIR="${PROJECT_DIR}/script"
+BUILD_SRC_DIR="${PROJECT_DIR}/ffmpeg"
+
+# unzip
+FF_TAR_NAME="ffmpeg-${VERSION}.tar.gz"
+echo "unzip ${FF_TAR_NAME} to ${BUILD_SRC_DIR} ..."
+tar zxf ${FF_TAR_NAME} -C "${BUILD_SRC_DIR}"
+cd "${BUILD_SRC_DIR}/ffmpeg-${VERSION}"
+
+for ARCH in ${ARCHS}
+do
+    echo ""
+    
+    SCRIPT_ARCH_NAME="config_${ARCH}.sh"
+    SCRIPT_MAKE_NAME="config_make.sh"
+    cp -f "${SCRIPTS_DIR}/${SCRIPT_ARCH_NAME}" .
+    cp -f "${SCRIPTS_DIR}/${SCRIPT_MAKE_NAME}" .
+    
+    # run
+    echo "executing ${SCRIPT_ARCH_NAME} ..."
+    sh "./${SCRIPT_ARCH_NAME}"
+done
+
